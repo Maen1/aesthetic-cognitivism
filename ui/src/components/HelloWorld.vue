f91ec97becc10244eeceb86bd4a35c649210fd51
maen1 <mk.maen93@gmail.com>
2024-09-12 15:22:57 +0200

    ui libraries

---
+++ ui/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { useCounterStore } from '../stores/ChartStore';
+import { ref } from 'vue'
+
+const store = useCounterStore()
+
+
+defineProps({
+  msg: String,
+})
+
+</script>
+
+<template>
+  <div class="w-full bg-teal-100 p-2 flex justify-center">
+      <h1 class="text-xl ">{{ msg }}</h1>
+  </div>
+
+
+  <div class="card">
+    <button class="bg-slate-100 rounded-full p-2" @click="store.increment">count is {{ store.doubleCount }}</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
