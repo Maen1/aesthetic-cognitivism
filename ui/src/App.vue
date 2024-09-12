0df2009369701dfbe9f9bcb331693c47998b85f6
maen1 <mk.maen93@gmail.com>
2024-09-12 16:40:22 +0200

    add new chart

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -43,39 +43,25 @@ onMounted(() => {
     });
     const ctx2 = document.getElementById('myChart2');
     new Chart(ctx2, {
-        type: 'bar',
+        type: 'doughnut',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: [
+                'Red',
+                'Blue',
+                'Yellow'
+            ],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 20, 5, 8, 3],
+                label: 'My First Dataset',
+                data: [30, 5, 10],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
                 ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
+                hoverOffset: 4
             }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
         }
-    });
+    });  
 })
 </script>
 
