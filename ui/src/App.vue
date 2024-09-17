625b73482ac1afd5305cf0b3e532a221b8c7aaac
maen1 <mk.maen93@gmail.com>
2024-09-17 12:15:53 +0200

    wip

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -13,8 +13,28 @@ onMounted(() => {
         data: {
             labels: ['Poetry', 'Concerts', 'Theater', 'Dance', 'Exhibitions', 'Operas'],
             datasets: [{
-                label: 'Category',
+                label: 'Beautiful',
                 data: [12, 19, 20, 5, 8, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            },{
+                label: 'profound',
+                data: [12, 1, 10, 15, 4, 3],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
@@ -52,7 +72,7 @@ onMounted(() => {
                 'Bach'
             ],
             datasets: [{
-                label: 'My First Dataset',
+                label: 'Dataset',
                 data: [30, 5, 10],
                 backgroundColor: [
                 'rgb(255, 99, 132)',
