37a5e87e1ec5491a6e35fa279712ecb6d5e86035
maen1 <mk.maen93@gmail.com>
2025-03-05 13:05:26 +0100

    use mongod docker

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,8 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
 # MONGO_URL = "mongodb://localhost:27017"
-MONGO_URL = "mongodb://host.docker.internal:27017"
+# MONGO_URL = "mongodb://host.docker.internal:27017"
+MONGO_URL = "mongodb://mongodb:27017"
 client = AsyncIOMotorClient(MONGO_URL)
 database = client["aesthetic"]
 criticism_collection = database["criticism"]
